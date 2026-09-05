c748e2d6610cc31c9ad8911092a6f28e1b040167
derrickweru <derrickweru73@gmail.com>
2026-09-05 16:30:30 +0300

    Add product API serializers

---
+++ backend/products/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+
+from .models import Category, Product, Inventory
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            "id",
+            "name",
+            "description",
+        ]
+
+
+class InventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = [
+            "id",
+            "quantity",
+            "updated_at",
+        ]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    inventory = InventorySerializer(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "description",
+            "price",
+            "category",
+            "inventory",
+            "is_active",
+            "created_at",
+        ]

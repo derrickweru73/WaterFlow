d125ae4753d1f03c9659a5890a719448f059f845
derrickweru <derrickweru73@gmail.com>
2026-09-05 14:39:52 +0300

    feat: update accounts/views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import RegisterSerializer
 from .serializers import RegisterSerializer
 
 
@@ -13,6 +13,8 @@ class RegisterView(generics.CreateAPIView):
 
 
 class ProtectedTestView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         return Response({
             "message": "JWT authentication is working",

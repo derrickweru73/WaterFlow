ba316a4bdd96dbd414ca0604621ae72598b24b46
derrickweru <derrickweru73@gmail.com>
2026-09-05 18:11:56 +0300

    Add inventory API URLs

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -7,15 +7,28 @@ from .views import (
     ProductCreateView,
     ProductUpdateView,
     ProductDeleteView,
+    InventoryListView,
+    InventoryDetailView,
+    InventoryCreateView,
+    InventoryUpdateView,
+    InventoryDeleteView,
 )
 
 urlpatterns = [
     path("categories/", CategoryListView.as_view(), name="category-list"),
 
     path("products/", ProductListView.as_view(), name="product-list"),
-    path("products/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+    path(
+        "products/<int:pk>/",
+        ProductDetailView.as_view(),
+        name="product-detail",
+    ),
 
-    path("management/products/", ProductCreateView.as_view(), name="product-create"),
+    path(
+        "management/products/",
+        ProductCreateView.as_view(),
+        name="product-create",
+    ),
     path(
         "management/products/<int:pk>/",
         ProductUpdateView.as_view(),
@@ -25,5 +38,32 @@ urlpatterns = [
         "management/products/<int:pk>/delete/",
         ProductDeleteView.as_view(),
         name="product-delete",
+    ),
+
+    path(
+        "inventory/",
+        InventoryListView.as_view(),
+        name="inventory-list",
+    ),
+    path(
+        "inventory/<int:pk>/",
+        InventoryDetailView.as_view(),
+        name="inventory-detail",
+    ),
+
+    path(
+        "management/inventory/",
+        InventoryCreateView.as_view(),
+        name="inventory-create",
+    ),
+    path(
+        "management/inventory/<int:pk>/",
+        InventoryUpdateView.as_view(),
+        name="inventory-update",
+    ),
+    path(
+        "management/inventory/<int:pk>/delete/",
+        InventoryDeleteView.as_view(),
+        name="inventory-delete",
     ),
 ]

e1c933f30b72cb156421f89508866dc0008b2b0d
derrickweru <derrickweru73@gmail.com>
2026-09-15 18:14:00 +0300

    Add subscription management to admin

--- backend/subscriptions/admin.py
+++ backend/subscriptions/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subscription, SubscriptionItem
+
+
+class SubscriptionItemInline(admin.TabularInline):
+    model = SubscriptionItem
+    extra = 0
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "customer",
+        "frequency",
+        "status",
+        "next_delivery_date",
+        "created_at",
+    )
+
+    list_filter = (
+        "frequency",
+        "status",
+        "next_delivery_date",
+    )
+
+    search_fields = (
+        "customer__username",
+        "customer__email",
+        "delivery_address",
+    )
+
+    inlines = [SubscriptionItemInline]

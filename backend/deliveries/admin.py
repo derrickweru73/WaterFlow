09c485a2f11ce3a65b7f3fdd0961378b0af75423
derrickweru <derrickweru73@gmail.com>
2026-09-15 18:07:00 +0300

    Add delivery management to admin

--- backend/deliveries/admin.py
+++ backend/deliveries/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Delivery
+
+
+@admin.register(Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "driver",
+        "status",
+        "delivery_address",
+        "assigned_at",
+        "delivered_at",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "created_at",
+    )
+
+    search_fields = (
+        "order__id",
+        "driver__username",
+        "delivery_address",
+    )
